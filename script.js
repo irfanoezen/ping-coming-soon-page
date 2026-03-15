163b3644650e2c136f8c10845906bbc82a3474ae
Irfan Oezen <197492804+irfanoezen@users.noreply.github.com>
2026-03-15 14:34:43 +0100

    Feat: clear input field after submitting valid input  or page refresh

--- script.js
+++ script.js
@@ -4,19 +4,38 @@ const form = document.querySelector("form");
 const input = document.querySelector("#input-field");
 
 const errorMessage = document.createElement("p");
+errorMessage.classList.add("error-message");
 errorMessage.textContent = "Please provide a valid email address";
 
+input.value = "";
+
 form.addEventListener("submit", (event) => {
-  if (input.validity.valueMissing || input.validity.typeMismatch || input.validity.patternMismatch) {
+  if (
+    input.validity.valueMissing ||
+    input.validity.typeMismatch ||
+    input.validity.patternMismatch
+  ) {
+    input.style.setProperty("border-color", "var(--red-400)");
+    input.style.setProperty("outline-color", "var(--red-400)");
     input.after(errorMessage);
+  } else {
+    form.reset();
   }
   event.preventDefault();
 });
 
 input.addEventListener("input", (event) => {
-  if (input.validity.valueMissing || input.validity.typeMismatch || input.validity.patternMismatch) {
+  if (
+    input.validity.valueMissing ||
+    input.validity.typeMismatch ||
+    input.validity.patternMismatch
+  ) {
+    input.style.setProperty("border-color", "var(--red-400)");
+    input.style.setProperty("outline-color", "var(--red-400)");
     event.target.after(errorMessage);
   } else if (event.target.nextElementSibling.tagName === "P") {
+    input.style.setProperty("border-color", "var(--blue-500)");
+    input.style.setProperty("outline-color", "var(--blue-500)");
     event.target.nextElementSibling.remove();
   }
 });
